60288e1450bb05ede7a16ab6207f11f0fc9d021c
Juan C. Poos <94860562+JuanCPoos@users.noreply.github.com>
2023-02-15 20:50:01 -0300

    header y app.js

---
+++ inventarymanag/src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default function Header(){ 
+    return (
+        <div className="container">
+            <h3>Soy el header</h3>
+        </div>
+    );
+}
+
