0a323e5ceb0e7615c36e31a557a0aa4fa4dbb5fe
Juan C. Poos <94860562+JuanCPoos@users.noreply.github.com>
2023-02-15 20:57:27 -0300

    se agregó Milligram

--- inventarymanag/src/components/header/Header.js
+++ inventarymanag/src/components/header/Header.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 export default function Header(){ 
     return (
-        <div className="container">
-            <h3>Soy el header</h3>
+        <div className="container">           
+             
+            
         </div>
     );
 }
