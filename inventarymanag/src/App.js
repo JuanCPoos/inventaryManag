a011c5457432006bef07530115ee257b6185577d
Juan C. Poos <94860562+JuanCPoos@users.noreply.github.com>
2023-02-16 20:42:20 -0300

    Maqueta login

--- inventarymanag/src/App.js
+++ inventarymanag/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header/Header';
 import BotonLogin from './components/BotonLogin';
+import Input from './components/login/Input';
 
 function App() {  
   return (
     <div className="">      
-      <BotonLogin />
+      {/* <BotonLogin /> */}
+      <Input />
     </div>
   );
 }
--- inventarymanag/src/components/BotonLogin.js
+++ inventarymanag/src/components/BotonLogin.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 export default function BotonLogin() { 
     return (
-        <div className='container App button__login'>
-            <a class="button" href="#">Ingresar</a>
+        <div className='container App box__login'>            
+            <div className='box'>
+                <h2>SGI</h2>
+                <h4>Para ingresar al sistema presione el botón</h4>
+                <a class="button" href="#">Ingresar</a>  
+            </div>
+            
         </div>
     );
 }
